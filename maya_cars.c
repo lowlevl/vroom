a1f592e95663fb006049fa7f57555602edaacaea
Maya the bee <hello@unw.re>
2024-11-24 19:19:52 +0100

    Done with exercise 2.

--- maya_cars.c
+++ maya_cars.c
@@ -1,8 +1,12 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <unistd.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define WIDTH 40
 
 void move_cursor(uint8_t line, uint8_t column)
 {
@@ -14,22 +18,22 @@ void clear_screen()
     fprintf(stdout, "\e[2J");
 }
 
-void game_init(uint8_t width)
+void game_init()
 {
     move_cursor(1, 1);
     clear_screen();
 
-    for (uint8_t col = 0; col < width; col++)
+    for (uint8_t column = 0; column < WIDTH; column++)
         fprintf(stdout, "-");
 
     fprintf(stdout, "\n\n");
 
-    for (uint8_t col = 0; col < width / 2; col++)
+    for (uint8_t column = 0; column < WIDTH / 2; column++)
         fprintf(stdout, "- ");
 
     fprintf(stdout, "\n\n");
 
-    for (uint8_t col = 0; col < width; col++)
+    for (uint8_t column = 0; column < WIDTH; column++)
         fprintf(stdout, "-");
 
     fflush(stdout);
@@ -43,11 +47,45 @@ void game_close()
     fflush(stdout);
 }
 
+// 0: top: <; 1: bottom: >
+void car_thread(bool route)
+{
+    char car = (route) ? '>' : '<';
+    uint8_t line = (route) ? 4 : 2;
+
+    uint8_t moves = 0;
+    while (moves < WIDTH)
+    {
+        uint8_t oldcolumn = (route) ? moves : WIDTH - moves + 1;
+        uint8_t column = (route) ? moves + 1 : WIDTH - moves;
+
+        move_cursor(line, oldcolumn);
+        fprintf(stdout, " ");
+
+        move_cursor(line, column);
+        fprintf(stdout, "%c", car);
+
+        move_cursor(1, WIDTH + 1);
+        fflush(stdout);
+
+        uint8_t ms = (rand() % 10 + 50);
+        usleep(ms * 1000);
+        ++moves;
+    }
+
+    move_cursor(line, (route) ? WIDTH : 1);
+    fprintf(stdout, " ");
+    fflush(stdout);
+}
+
 int main(int argc, char **argv)
 {
-    game_init(40);
+    srand(time(NULL));
 
-    sleep(3);
+    game_init();
+
+    car_thread(0);
+    car_thread(1);
 
     game_close();
 }
