300eb2baac60f0ca825b238c4e1e4ad52526cb1f
Maya the bee <hello@unw.re>
2024-11-24 18:47:42 +0100

    Done with exercise 1.

--- maya_cars.c
+++ maya_cars.c
@@ -1,19 +1,51 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 
-void game_init()
+void move_cursor(uint8_t line, uint8_t column)
 {
+    fprintf(stdout, "\e[%d;%dH", line, column);
+}
+
+void clear_screen()
+{
+    fprintf(stdout, "\e[2J");
+}
+
+void game_init(uint8_t width)
+{
+    move_cursor(1, 1);
+    clear_screen();
+
+    for (uint8_t col = 0; col < width; col++)
+        fprintf(stdout, "-");
+
+    fprintf(stdout, "\n\n");
+
+    for (uint8_t col = 0; col < width / 2; col++)
+        fprintf(stdout, "- ");
+
+    fprintf(stdout, "\n\n");
+
+    for (uint8_t col = 0; col < width; col++)
+        fprintf(stdout, "-");
+
+    fflush(stdout);
 }
 
 void game_close()
 {
+    move_cursor(1, 1);
+    clear_screen();
+
+    fflush(stdout);
 }
 
 int main(int argc, char **argv)
 {
-    game_init();
+    game_init(40);
 
     sleep(3);
 
